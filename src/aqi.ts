58f88d622c21d1b5e692eb3539a61a2be9c1a369
Jakub Synowiec <github@jakubsynowiec.info>
2017-11-16 23:02:17 +0100

    Add AQI values

---
+++ src/aqi.ts
@@ -0,0 +1,27 @@
+export const AQI_INDEX = [
+  {
+    min: 0,
+    max: 25,
+    label: 'Very low',
+  },
+  {
+    min: 25,
+    max: 50,
+    label: 'Low',
+  },
+  {
+    min: 50,
+    max: 75,
+    label: 'Medium',
+  },
+  {
+    min: 75,
+    max: 100,
+    label: 'High',
+  },
+  {
+    min: 100,
+    max: Number.MAX_VALUE,
+    label: 'Very High',
+  },
+];
