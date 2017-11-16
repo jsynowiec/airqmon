edda859dc35c8084a43ac787aad39a9feacb5275
Jakub Synowiec <github@jakubsynowiec.info>
2017-11-16 23:00:04 +0100

    Add Airly API response type defs

---
+++ src/airly.ts
@@ -0,0 +1,31 @@
+export interface IAirlyCurrentMeasurement {
+  airQualityIndex: number;
+  pm1: number;
+  pm25: number;
+  pm10: number;
+  pressure: number;
+  humidity: number;
+  temperature: number;
+  pollutionLevel: number;
+}
+export interface IArilyNearestSensorMeasurement {
+  airQualityIndex: number;
+  pm25: number;
+  pm10: number;
+  distance: number;
+  id: number;
+  name: string;
+  vendor: string;
+  location: {
+    latitude: number;
+    longitude: number;
+  };
+  pollutionLevel: number;
+  address: {
+    streetNumber: number;
+    route: string;
+    locality: string;
+    country: string;
+  };
+  measurementTime: string;
+}
