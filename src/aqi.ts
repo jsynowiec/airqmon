d0e13c45f6e22da3825782cabfa86a04e5d108e6
Jakub Synowiec <github@jakubsynowiec.info>
2017-11-17 08:50:17 +0100

    Move AQI values to a separate key

--- src/aqi.ts
+++ src/aqi.ts
@@ -1,33 +1,44 @@
 export const AQI_INDEX = [
   {
-    min: 0,
-    max: 25,
+    values: {
+      min: 0,
+      max: 25,
+    },
     label: 'Very low',
+
   },
   {
-    min: 25,
-    max: 50,
+    values: {
+      min: 25,
+      max: 50,
+    },
     label: 'Low',
   },
   {
-    min: 50,
-    max: 75,
+    values: {
+      min: 50,
+      max: 75,
+    },
     label: 'Medium',
   },
   {
-    min: 75,
-    max: 100,
+    values: {
+      min: 75,
+      max: 100,
+    },
     label: 'High',
   },
   {
-    min: 100,
-    max: Number.MAX_VALUE,
+    values: {
+      min: 100,
+      max: Number.MAX_VALUE,
+    },
     label: 'Very High',
   },
 ];
 
 export function humanize(aqi: number): string {
   return AQI_INDEX.find((elem) => {
-    return aqi >= elem.min && aqi < elem.max;
+    return aqi >= elem.values.min && aqi < elem.values.max;
   }).label;
 }
