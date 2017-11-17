bf6265148c384967d42ed6c685b323925ffc94cd
Jakub Synowiec <github@jakubsynowiec.info>
2017-11-17 15:24:00 +0100

    Refactor Content, extract MeasurementPane component

--- src/TrayWindow/Content.tsx
+++ src/TrayWindow/Content.tsx
@@ -1,15 +1,8 @@
 import * as React from 'react';
 
 import { IAirlyCurrentMeasurement, IArilyNearestSensorMeasurement } from '../airly';
-
-import Measurement from './Measurement';
-
+import MeasurementPane from './MeasurementPane';
 import { humanize } from '../caqi';
-
-const formatters = {
-  toFixed2: (val: number) => val.toFixed(2),
-  divBy100ToFixed2: (val: number) => (val / 100).toFixed(2),
-};
 
 interface ITrayContentProps {
   currentMeasurements?: IAirlyCurrentMeasurement;
@@ -22,53 +15,7 @@ const TrayContent = ({ currentMeasurements, nearestStation }: ITrayContentProps)
       <div className="window-content">
         <div className="pane">
 
-          <div className="measurement-pane">
-            <Measurement
-              description="CAQI"
-              reading={currentMeasurements.airQualityIndex}
-              formatter={formatters.toFixed2}
-            />
-            <Measurement
-              description="Pollution level"
-              reading={currentMeasurements.pollutionLevel}
-            />
-            <Measurement
-              description="PM1"
-              unit="μg/m<sup>3</sup>"
-              reading={currentMeasurements.pm1}
-              formatter={formatters.toFixed2}
-            />
-            <Measurement
-              description="PM2.5"
-              unit="μg/m<sup>3</sup>"
-              reading={currentMeasurements.pm25}
-              formatter={formatters.toFixed2}
-            />
-            <Measurement
-              description="PM10"
-              unit="μg/m<sup>3</sup>"
-              reading={currentMeasurements.pm10}
-              formatter={formatters.toFixed2}
-            />
-            <Measurement
-              description="Temperature"
-              unit="<sup>&deg;</sup>C"
-              reading={currentMeasurements.temperature}
-              formatter={formatters.toFixed2}
-            />
-            <Measurement
-              description="Pressure"
-              unit="hPa"
-              reading={currentMeasurements.pressure}
-              formatter={formatters.divBy100ToFixed2}
-            />
-            <Measurement
-              description="Humidity"
-              unit="%"
-              reading={currentMeasurements.humidity}
-              formatter={formatters.toFixed2}
-            />
-          </div>
+          <MeasurementPane measurement={currentMeasurements} />
 
           <div className="summary">
             Air pollution is&nbsp;
---
+++ src/TrayWindow/MeasurementPane.tsx
@@ -0,0 +1,69 @@
+import * as React from 'react';
+
+import { IAirlyCurrentMeasurement } from '../airly';
+
+import { MeasurementValue } from './Measurements/MeasurementValue';
+import { Unit } from './Measurements/MeasurementReadingUnit';
+
+const formatters: { [key: string]: (val: string|number) => string } = {
+  toFixed2: (val: number) => val.toFixed(2),
+  divBy100ToFixed2: (val: number) => (val / 100).toFixed(2),
+};
+
+interface IMeasurementPaneProps {
+  measurement?: IAirlyCurrentMeasurement;
+}
+
+const MeasurementPane = ({ measurement }: IMeasurementPaneProps) => {
+  return (
+    <div className="measurement-pane">
+      <MeasurementValue
+        description="CAQI"
+        reading={measurement.airQualityIndex}
+        formatter={formatters.toFixed2}
+      />
+      <MeasurementValue
+        description="Pollution level"
+        reading={measurement.pollutionLevel}
+      />
+      <MeasurementValue
+        description="PM1"
+        unit={Unit.PM}
+        reading={measurement.pm1}
+        formatter={formatters.toFixed2}
+      />
+      <MeasurementValue
+        description="PM2.5"
+        unit={Unit.PM}
+        reading={measurement.pm25}
+        formatter={formatters.toFixed2}
+      />
+      <MeasurementValue
+        description="PM10"
+        unit={Unit.PM}
+        reading={measurement.pm10}
+        formatter={formatters.toFixed2}
+      />
+      <MeasurementValue
+        description="Temperature"
+        unit={Unit.TEMP_C}
+        reading={measurement.temperature}
+        formatter={formatters.toFixed2}
+      />
+      <MeasurementValue
+        description="Pressure"
+        unit={Unit.PRESSURE_PA}
+        reading={measurement.pressure}
+        formatter={formatters.divBy100ToFixed2}
+      />
+      <MeasurementValue
+        description="Humidity"
+        unit={Unit.PERCENT}
+        reading={measurement.humidity}
+        formatter={formatters.toFixed2}
+      />
+    </div>
+  );
+};
+
+export default MeasurementPane;
