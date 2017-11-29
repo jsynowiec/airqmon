aa7748066d1bbc79c3df39d96586861adb8b43f9
Jakub Synowiec <github@jakubsynowiec.info>
2017-11-29 07:05:56 +0100

    Change layout

--- src/tray-window/StationInfo.tsx
+++ src/tray-window/StationInfo.tsx
@@ -3,14 +3,17 @@ import * as React from 'react';
 const StationInfo = ({ station, onClickHandler }) => {
   return (
     <div className="summary small">
-      Distance to station {(station.distance / 1000).toFixed(2)} km<br/>
       <a
         className="link"
         href="#"
         onClick={onClickHandler}
       >
+      <span className="icon icon-direction" />&nbsp;
       {`${station.address.locality}, ${station.address.route}`}
       </a>
+      <p>
+        Distance to station {(station.distance / 1000).toFixed(1)} km<br/>
+      </p>
     </div>
   );
 };
