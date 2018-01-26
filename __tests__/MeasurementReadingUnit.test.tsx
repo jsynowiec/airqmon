ec281b7dd7c1f30d803b2182715ca8687556efe3
Jakub Synowiec <github@jakubsynowiec.info>
2018-01-26 22:23:58 +0100

    Switch to describe -> it syntax

--- __tests__/MeasurementReadingUnit.test.tsx
+++ __tests__/MeasurementReadingUnit.test.tsx
@@ -2,28 +2,31 @@ import * as React from 'react';
 import { shallow } from 'enzyme';
 import { Unit, MeasurementReadingUnit } from '../src/tray-window/measurement/MeasurementReadingUnit'
 
-test('MeasurementReadingUnit renders proper unit for Unit.PERCENT', () => {
-  const wrapper = shallow(<MeasurementReadingUnit unit={Unit.PERCENT} />);
-  expect(wrapper.text()).toMatchSnapshot();
+describe('MeasurementReadingUnit', () => {
+  it('renders proper unit for Unit.PERCENT', () => {
+    const wrapper = shallow(<MeasurementReadingUnit unit={Unit.PERCENT} />);
+    expect(wrapper.text()).toMatchSnapshot();
+  });
+
+  it('renders proper unit for Unit.PM', () => {
+    const wrapper = shallow(<MeasurementReadingUnit unit={Unit.PM} />);
+    expect(wrapper.text()).toMatchSnapshot();
+  });
+
+  it('renders proper unit for Unit.PRESSURE_PA', () => {
+    const wrapper = shallow(<MeasurementReadingUnit unit={Unit.PRESSURE_PA} />);
+    expect(wrapper.text()).toMatchSnapshot();
+  });
+
+  it('renders proper unit for Unit.TEMP_C', () => {
+    const wrapper = shallow(<MeasurementReadingUnit unit={Unit.TEMP_C} />);
+    expect(wrapper.text()).toMatchSnapshot();
+  });
+
+  it('doesn\'t render if no unit is given', () => {
+    const wrapper = shallow(<MeasurementReadingUnit />);
+    expect(wrapper.getElement()).toBeNull();
+  });
 });
 
-test('MeasurementReadingUnit renders proper unit for Unit.PM', () => {
-  const wrapper = shallow(<MeasurementReadingUnit unit={Unit.PM} />);
-  expect(wrapper.text()).toMatchSnapshot();
-});
-
-test('MeasurementReadingUnit renders proper unit for Unit.PRESSURE_PA', () => {
-  const wrapper = shallow(<MeasurementReadingUnit unit={Unit.PRESSURE_PA} />);
-  expect(wrapper.text()).toMatchSnapshot();
-});
-
-test('MeasurementReadingUnit renders proper unit for Unit.TEMP_C', () => {
-  const wrapper = shallow(<MeasurementReadingUnit unit={Unit.TEMP_C} />);
-  expect(wrapper.text()).toMatchSnapshot();
-});
-
-test('MeasurementReadingUnit doesn\'t render if no unit is given', () => {
-  const wrapper = shallow(<MeasurementReadingUnit />);
-  expect(wrapper.getElement()).toBeNull();
-});
 
