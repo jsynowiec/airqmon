7f293014686e9417212aab79bc3ea608e0d14d20
Jakub Synowiec <github@jakubsynowiec.info>
2018-02-27 22:47:17 +0100

    Allow to use cached position but not older than 5 minutes
    5 minutes is a reasonable time. In real world a user usually can't cover enough distance to make a difference.

--- src/geolocation.ts
+++ src/geolocation.ts
@@ -1,7 +1,9 @@
 import { getCurrentPosition } from 'macos-location';
 
+const MAXIMUM_AGE: number = 5 * 60 * 1000; // 5 minutes in ms
+
 export function getLocation(): Promise<Position> {
   return new Promise((resolve, reject) => {
-    getCurrentPosition(resolve, reject);
+    getCurrentPosition(resolve, reject, { maximumAge: MAXIMUM_AGE });
   });
 }
