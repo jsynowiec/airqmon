1c5fdce82a4481e509f6f5413986dad87b3f1a25
Jakub Synowiec <github@jakubsynowiec.info>
2018-03-07 22:08:18 +0100

    Switch to ts-jest from simple ts preprocessor

--- jest.config.js
+++ jest.config.js
@@ -5,13 +5,15 @@ module.exports = {
     'enzyme-to-json/serializer',
   ],
   transform: {
-    '.(ts|tsx)': '<rootDir>/preprocessor.js'
+    '^.+\\.tsx?$': 'ts-jest'
   },
   moduleFileExtensions: [
-    'ts',
-    'tsx',
-    'js',
-    'jsx',
+    "ts",
+    "tsx",
+    "js",
+    "jsx",
+    "json",
+    "node"
   ],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
