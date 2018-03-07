23768f2bea039d1226881563296b3c39b5a15f39
Jakub Synowiec <github@jakubsynowiec.info>
2018-03-07 11:21:59 +0100

    Add contaminates tresholds and norms

---
+++ __tests__/contamination.test.ts
@@ -0,0 +1,40 @@
+import { getContaminationThresholdIndex } from '../src/contamination';
+
+const getContaminationThresholdIndexForPM10 = (value: number) => {
+  return getContaminationThresholdIndex('PM10', value);
+}
+
+describe('test getContaminationThresholdIndex helper', () => {
+  describe('for PM10 thresholds', () => {
+    it('returns index == 0 for values <0, 20>', () => {
+      expect(getContaminationThresholdIndexForPM10(0)).toBe(0);
+      expect(getContaminationThresholdIndexForPM10(19)).toBe(0);
+    });
+
+    it('returns index == 1 for values (20, 60>', () => {
+      expect(getContaminationThresholdIndexForPM10(21)).toBe(1);
+      expect(getContaminationThresholdIndexForPM10(60)).toBe(1);
+    });
+
+    it('returns index == 2 for values (60, 100>', () => {
+      expect(getContaminationThresholdIndexForPM10(61)).toBe(2);
+      expect(getContaminationThresholdIndexForPM10(100)).toBe(2);
+    });
+
+    it('returns index == 3 for values (100, 140>', () => {
+      expect(getContaminationThresholdIndexForPM10(101)).toBe(3);
+      expect(getContaminationThresholdIndexForPM10(140)).toBe(3);
+    });
+
+    it('returns index == 4 for values (140, 200>', () => {
+      expect(getContaminationThresholdIndexForPM10(141)).toBe(4);
+      expect(getContaminationThresholdIndexForPM10(200)).toBe(4);
+    });
+
+    it('returns index == 5 for values >200', () => {
+      expect(getContaminationThresholdIndexForPM10(201)).toBe(5);
+      expect(getContaminationThresholdIndexForPM10(250)).toBe(5);
+      expect(getContaminationThresholdIndexForPM10(500)).toBe(5);
+    });
+  });
+});
---
+++ src/contamination.ts
@@ -0,0 +1,107 @@
+export enum Contaminates {
+  PM1 = 'PM1',
+  PM10 = 'PM10',
+  PM25 = 'PM25',
+}
+
+export const CONTAMINATION_THRESHOLDS = {
+  [Contaminates.PM10]: [
+    {
+      index: 0,
+      values: {
+        min: 0,
+        max: 20,
+      },
+    },
+    {
+      index: 1,
+      values: {
+        min: 20,
+        max: 60,
+      },
+    },
+    {
+      index: 2,
+      values: {
+        min: 60,
+        max: 100,
+      },
+    },
+    {
+      index: 3,
+      values: {
+        min: 100,
+        max: 140,
+      },
+    },
+    {
+      index: 4,
+      values: {
+        min: 140,
+        max: 200,
+      },
+    },
+    {
+      index: 5,
+      values: {
+        min: 200,
+        max: Number.MAX_VALUE,
+      },
+    },
+  ],
+  [Contaminates.PM25]: [
+    {
+      index: 0,
+      values: {
+        min: 0,
+        max: 12,
+      },
+    },
+    {
+      index: 1,
+      values: {
+        min: 12,
+        max: 36,
+      },
+    },
+    {
+      index: 2,
+      values: {
+        min: 36,
+        max: 60,
+      },
+    },
+    {
+      index: 3,
+      values: {
+        min: 60,
+        max: 84,
+      },
+    },
+    {
+      index: 4,
+      values: {
+        min: 84,
+        max: 120,
+      },
+    },
+    {
+      index: 5,
+      values: {
+        min: 120,
+        max: Number.MAX_VALUE,
+      },
+    },
+  ],
+};
+
+export const CONTAMINATION_NORM_VALUES = {
+  [Contaminates.PM10]: 25,
+  [Contaminates.PM25]: 50,
+};
+
+export function getContaminationThresholdIndex(contaminate: Contaminates, val: number): number {
+  return CONTAMINATION_THRESHOLDS[contaminate].find((elem) => {
+    return val >= elem.values.min && val <= elem.values.max;
+  }).index;
+}
