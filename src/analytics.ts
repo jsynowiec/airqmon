3b363dda0ea86958aae8a6b6e84ed045f1d7d490
Jakub Synowiec <github@jakubsynowiec.info>
2019-03-22 14:31:39 +0100

    Use dummy analytics when in dev mode

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,6 +1,15 @@
 import * as ua from 'universal-analytics';
+import { isDev } from './helpers';
 
 const keys = require('../keys.json');
 
 const visitor = ua(keys.ganalytics);
+
+if (isDev()) {
+  const dummy = () => ({ send: () => {} });
+
+  visitor.event = dummy;
+  visitor.screenview = dummy;
+}
+
 export default visitor;
