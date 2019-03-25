ed86e7aafc32a75933fc238bc1ee9e957de4919e
Jakub Synowiec <github@jakubsynowiec.info>
2019-03-25 23:39:02 +0100

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
-  setupTestFrameworkScriptFile: '<rootDir>/setupTests.js',
-  snapshotSerializers: [
-    'enzyme-to-json/serializer',
-  ],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
   transform: {
-    '^.+\\.tsx?$': 'ts-jest'
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx,js,jsx}',
-    '!src/**/*.d.ts',
-  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx,js,jsx}', '!src/**/*.d.ts'],
 };
