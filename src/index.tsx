8878793f490af1a197eb5f81d2bed7520b08f7be
Jakub Synowiec <github@jakubsynowiec.info>
2019-04-22 21:16:41 +0200

    Merge online-status with main window renderer

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,21 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
+import { ipcRenderer } from 'electron';
+
 import { getVisitor } from 'common/analytics';
+import IPC_EVENTS from 'common/ipc-events';
+
 import { App } from './app';
+
+function updateOnlineStatus(): void {
+  ipcRenderer.send(IPC_EVENTS.CONN_STATUS_CHANGED, navigator.onLine ? 'online' : 'offline');
+}
+
+window.addEventListener('online', updateOnlineStatus);
+window.addEventListener('offline', updateOnlineStatus);
+
+updateOnlineStatus();
 
 getVisitor()
   .screenview('Tray window', 'Airqmon')
