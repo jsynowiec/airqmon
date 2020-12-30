2c89efc7478a18738bed27c8c93545a16de52696
Jakub Synowiec <github@jakubsynowiec.info>
2020-12-30 18:51:36 +0100

    Build for Apple Silicon

--- bin/pack.js
+++ bin/pack.js
@@ -2,21 +2,26 @@ const packager = require('electron-packager');
 const path = require('path');
 const pkg = require('../package');
 
-packager({
-  dir: path.join(__dirname, '..'),
-  name: 'Airqmon',
-  appBundleId: `app.airqmon`,
-  icon: path.join(__dirname, '..', 'assets', 'airqmon'),
-  appCategoryType: 'public.app-category.weather',
-  ignore: ['^/src', '^/__tests__', '^/coverage'],
-  asar: true,
-  overwrite: true,
-  packageManager: 'yarn',
-  out: path.join(__dirname, '..', 'out'),
-  arch: 'x64',
-  platform: 'darwin',
-  darwinDarkModeSupport: true,
-  electronVersion: pkg.electronVersion,
-}).then((res) => {
-  process.exit(0);
-});
+async function bundleElectronApp(options) {
+  const appPaths = await packager(options);
+  console.log(`Electron app bundles created:\n${appPaths.join('\n')}`);
+}
+
+(async () => {
+  await bundleElectronApp({
+    dir: path.join(__dirname, '..'),
+    name: 'Airqmon',
+    appBundleId: `app.airqmon`,
+    icon: path.join(__dirname, '..', 'assets', 'airqmon.icns'),
+    appCategoryType: 'public.app-category.weather',
+    ignore: ['^/src', '^/__tests__', '^/coverage'],
+    asar: true,
+    overwrite: true,
+    packageManager: 'yarn',
+    out: path.join(__dirname, '..', 'out'),
+    arch: ['x64', 'arm64'],
+    platform: 'darwin',
+    darwinDarkModeSupport: true,
+    electronVersion: pkg.electronVersion,
+  });
+})();
